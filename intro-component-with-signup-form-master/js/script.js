aa998f6e3dcac6da489ee52fce9ea46ce9f64803
paulauskas-linas <paulauskas.linas@gmail.com>
2022-07-27 07:33:00 +0300

    add validation; add error states

---
+++ intro-component-with-signup-form-master/js/script.js
@@ -0,0 +1,18 @@
+const patterns = {
+ email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+ fname:/(?!^$)([^\s])/, //not empty, no spaces
+ lname:/(?!^$)([^\s])/,
+ password:/(?!^$)([^\s])/,
+}    
+const inputs = document.querySelectorAll("input");
+
+function validate(input, regex){
+    return regex.test(input.value) ? input.className = 'valid' : input.className = 'invalid'
+}
+
+inputs.forEach(input => {
+    input.addEventListener("blur", (e) => {
+        validate(e.target,patterns[e.target.attributes.name.value]);
+        });
+});
+
